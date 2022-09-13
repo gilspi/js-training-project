a65268102c7e44d8772eb793defa14b1dba106fa
gilspi <gilspi@email.con>
2022-09-13 20:17:04 +0300

    Alive modal window with 'classList.add and classList.remove' class 'hide and show'. Thats the frist var to complete the task.

--- js/script.js
+++ js/script.js
@@ -94,4 +94,21 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     setClock('.timer', deadline);
+
+    // Modal window
+    const modalTrigger = document.querySelector('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.addEventListener('click', () => {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+    });
+
+    modalCloseBtn.addEventListener('click', () => {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    });
 });
